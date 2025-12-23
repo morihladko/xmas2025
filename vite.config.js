f8d9309702782240373143ab2c7c27077c12bfc0
Peter Morihladko <morihladko@gmail.com>
2025-12-23 17:40:31 +0100

    Add github actions

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.glb'],
-  // For GitLab Pages: use repo name as base path in production
-  base: process.env.CI ? `/${process.env.CI_PROJECT_NAME}/` : '/',
+  // For GitHub Pages: use repo name as base path in production
+  base: '/xmas2025/',
 })
